273bd6b720f4dfce5597a2971ec61dbaf564ca1b
Rick Companje <rick@companje.nl>
2012-05-27 15:13:55 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "ofxExtras.h"
+
+//global
+
+ofxBeginApp();
+	
+
+void setup() {
+
+}
+
+void update() {
+    
+}
+
+void draw() {
+    
+}
+
+void mousePressed(int x, int y, int button) {
+    
+}
+
+ofxEndApp();
